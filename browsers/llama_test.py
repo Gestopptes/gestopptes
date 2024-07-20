76e2fb2ba5b51e2bdaa5054589eec90ab3d113eb
Oodapow <petre.v.alexandru@gmail.com>
2024-07-20 21:30:12 +0300

    add llama create

--- browsers/llama_test.py
+++ browsers/llama_test.py
@@ -2,7 +2,7 @@
 from src.tasks.lama_index import lama_index_demo
 
 
-BASE_URL = 'https://github.com/langchain-ai/langchain/tree/langchain%3D%3D0.2.6/templates/csv-agent'
+BASE_URL = 'https://neo4j.com/labs/genai-ecosystem/llamaindex/'
 
 
 if __name__ == "__main__":
