0d1869968b5bad9ac53bccec7405577268590bbf
Oodapow <petre.v.alexandru@github.com>
2024-07-21 06:14:54 +0300

    fix tempo

--- browsers/src/workflows/lama_index.py
+++ browsers/src/workflows/lama_index.py
@@ -7,7 +7,7 @@ from ._helpers import execute_child_workflow, execute_workflow, execute_activity
 @workflow.defn
 class LamaIndexDemoWorkflow:
     @staticmethod
-    def make_id(url):
+    def make_id(url, options):
         return SCRAPE_TASK_VERSION+"__lama_demo_v2__"+url
     
     @workflow.run
