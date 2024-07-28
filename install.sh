bbb40cce0120ab0bdd168151afd9c2bbe726015a
Oodapow <petre.v.alexandru@github.com>
2024-07-29 01:05:03 +0300

    vllm dont work

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 set -ex
 
-(
-cd browsers
-pipenv install
-)
+# (
+# cd browsers
+# pipenv install
+# )
 
 
 docker network create hekthon || true
@@ -18,16 +18,16 @@ docker network create hekthon || true
     COMPOSE_PROJECT_NAME=docker-services docker-compose up -d --remove-orphans
 )
 
-(
-    cd docker-services/docker-tempo
-    # COMPOSE_PROJECT_NAME=temporal docker-compose down -t 1 --remove-orphans -v || true
-    # COMPOSE_PROJECT_NAME=temporal docker-compose rm -f -v || true
-    # docker volume rm -f   temporal_temporalio_cass    temporal_temporalio_es || true
-    COMPOSE_PROJECT_NAME=temporal docker-compose -f docker-compose-cass-es.yml up -d --remove-orphans
-)
+# (
+#     cd docker-services/docker-tempo
+#     # COMPOSE_PROJECT_NAME=temporal docker-compose down -t 1 --remove-orphans -v || true
+#     # COMPOSE_PROJECT_NAME=temporal docker-compose rm -f -v || true
+#     # docker volume rm -f   temporal_temporalio_cass    temporal_temporalio_es || true
+#     COMPOSE_PROJECT_NAME=temporal docker-compose -f docker-compose-cass-es.yml up -d --remove-orphans
+# )
 
-(
-    cd browsers
-    COMPOSE_PROJECT_NAME=browsers docker-compose down -t 1 || true
-    COMPOSE_PROJECT_NAME=browsers docker-compose -f docker-compose.yml up -d --remove-orphans --build
-)
+# (
+#     cd browsers
+#     COMPOSE_PROJECT_NAME=browsers docker-compose down -t 1 || true
+#     COMPOSE_PROJECT_NAME=browsers docker-compose -f docker-compose.yml up -d --remove-orphans --build
+# )
